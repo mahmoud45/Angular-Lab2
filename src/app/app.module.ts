aa17e87482df48fdb02df360bf5d9918d84d1b6d
mahmoud45 <magdymahmoud913@gmail.com>
2023-10-11 01:24:55 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { Task1Component } from './Components/task1/task1.component';
+import { Task2Component } from './Components/task2/task2.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        Task1Component,
+        Task2Component,
+    ],
+    imports: [
+        FormsModule,
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
